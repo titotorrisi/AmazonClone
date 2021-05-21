44f9647ff909c3c4a807924cbc543a3a98bc5c96
titotorrisi <titotorrisi2@gmail.com>
2021-05-21 01:49:37 -0400

    Finished Mobile Responsive UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,19 +22,58 @@ const Header = (props) => {
 
                 {/*Search Products */}
                 <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
-                    <input type="text" className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none"/>
+                    <input type="text" className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"/>
                     <SearchIcon className="h-12 p-4"/>
+                </div>
 
+            {/*    Right Section */}
+
+                <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+                    <div className=" link">
+                        <p> Hello, Aarjav Chandarana </p>
+                        <p className="font-extrabold md:text-sm"><strong> Account & Lists </strong></p>
+                    </div>
+
+                    <div className=" link">
+                        <p>Returns</p>
+                        <p className="font-extrabold md:text-sm">& Orders</p>
+                    </div>
+
+                    <div className="relative link flex items-center">
+
+                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+
+                        <ShoppingCartIcon className="h-10"/>
+                        <p className="hidden md:inline font-extrabold md:text-sm mt-2 ">Cart</p>
+                    </div>
                 </div>
 
 
             </div>
 
             {/*Bottom Nav*/}
-            <div>
+            <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-xs">
+                <p className="link flex items-center">
+                    <MenuIcon className="h-6 mr-1"/>
+                    All
+                </p>
+                <p className="link">Prime Video</p>
+                <p className="link">Amazon Business</p>
+                <p className="link">Today's Deals</p>
+                <p className="link hidden lg:inline-flex">Electronics</p>
+                <p className="link hidden lg:inline-flex">Food & Grocery</p>
+                <p className="link hidden lg:inline-flex">Prime</p>
+                <p className="link hidden lg:inline-flex">Buy again</p>
+                <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+                <p className="link hidden lg:inline-flex">Health & Personal Care</p>
 
+
+
+
+                <p></p>
+                <p></p>
             </div>
-            <h1>I am a header</h1>
+
         </header>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({products}) {
+    console.log(products);
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
@@ -12,7 +15,25 @@ export default function Home() {
 
 
         <Header />
+
+        <main className="max-w-screen-2xl mx-auto">
+        {/*    banner */}
+            <Banner />
+        {/*    Product feed */}
+            <ProductFeed products={products} />
+        </main>
     {/*    Scrollable Banner */}
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+   const products = await fetch("https://fakestoreapi.com/products")
+       .then( (res) => res.json()
+       );
+
+   return  {
+       props : {
+            products,
+       }}
 }
